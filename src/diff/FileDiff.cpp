680e5f3a5692444965b67c900a74b4e1aa84355e
Simon Wagner <wagner.sim1988@googlemail.com>
2013-09-19 18:20:55 +0200

    FIX also parse hunk headers where hunk size is missing
    "@@ +1,4 -3 @@" is also a valid header. If the size is missing, it defaults to 1.

--- src/diff/FileDiff.cpp
+++ src/diff/FileDiff.cpp
@@ -11,7 +11,7 @@ using namespace std;
 Maybe<QSharedPointer<FileDiff>> FileDiff::createFromString(QString diff)
 {
     static const QRegularExpression newLineRE = QRegularExpression(R"(\n|(\r\n))");
-    static const QRegularExpression hunkStartRE = QRegularExpression(R"(@@\s+-(\d+),(\d+)\s+\+(\d+),(\d+)\s+@@(\s(.*))?)");
+    static const QRegularExpression hunkStartRE = QRegularExpression(R"(@@\s+-(\d+)(,(\d+))?\s+\+(\d+)(,(\d+))?\s+@@(\s(.*))?)");
     QStringList lines = diff.split(newLineRE);
     HunksList hunks;
     unique_ptr<HunkBuilder> currentHunkBuilder = nullptr;
@@ -31,16 +31,18 @@ Maybe<QSharedPointer<FileDiff>> FileDiff::createFromString(QString diff)
 
             diffrange oldRange, newRange;
             oldRange.start = hunkStartMatch.captured(1).toInt();
-            oldRange.length = hunkStartMatch.captured(2).toInt();
-            newRange.start = hunkStartMatch.captured(3).toInt();
-            newRange.length = hunkStartMatch.captured(4).toInt();
+            QStringRef strOldRangeLength = hunkStartMatch.capturedRef(3);
+            oldRange.length = strOldRangeLength.isNull() ? 1 : strOldRangeLength.toInt();
+            newRange.start = hunkStartMatch.captured(4).toInt();
+            QStringRef strNewRangeLength = hunkStartMatch.capturedRef(6);
+            newRange.length = strNewRangeLength.isNull() ? 1 : strNewRangeLength.toInt();
 
             currentHunkBuilder = unique_ptr<HunkBuilder>(new HunkBuilder(oldRange, newRange));
 
             //if we have a rest after the hunk, add it as context
-            if(!hunkStartMatch.capturedRef(6).isNull())
+            if(!hunkStartMatch.capturedRef(8).isNull())
             {
-                currentHunkBuilder->addHeader(hunkStartMatch.captured(6));
+                currentHunkBuilder->addHeader(hunkStartMatch.captured(8));
             }
         }
         //if it is not the start of a hunk, it is either a context line or a change
